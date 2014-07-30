ee3fef92a4621382639f3a40fdd9f2f58f5458ec
Ryan Mott <rymo@298223.no-reply.drupal.org>
2014-07-30 13:14:00 -0500

    experiment: less code. result: less reliable; sporadic iframe name changes - weird article shenanigans?

--- 7-30-2014/assets/js/app.js
+++ 7-30-2014/assets/js/app.js
@@ -1,21 +1,14 @@
 // All JS code goes in this file.
 
 function reqListener () {
-    //console.log(this.responseXML); //.responseText);
-    var xml = this.responseXML;
-    var items = xml.getElementsByTagName('item');
-    console.log(items);
     var nav = document.getElementById('nav');
+    var items = this.responseXML.getElementsByTagName('item');
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
-        console.log(item);
         var link = document.createElement('a');
         link.href = item.getElementsByTagName('link')[0].innerHTML;
         link.innerHTML = item.getElementsByTagName('title')[0].innerHTML;
-        link.onclick = function() {
-          document.getElementById('iftarget').src = this.href;
-            return false;
-        };
+        link.target = "iftargetname";
         nav.appendChild(link);
         nav.appendChild(document.createElement('br'));
     }
